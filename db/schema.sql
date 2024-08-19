86964132e9070f170106687282493fb4753ff79e
Cong Wang <wangcong525@hotmail.com>
2024-08-19 17:32:01 -0400

    schema syntax fixed

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,4 @@
+-- Active: 1723506150529@@127.0.0.1@5432@employee_db
 DROP DATABASE IF EXISTS employee_db;
 
 CREATE DATABASE employee_db;
@@ -12,13 +13,16 @@ CREATE TABLE role (
   id SERIAL PRIMARY KEY,
   title VARCHAR(30) UNIQUE NOT NULL,
   salary DECIMAL NOT NULL,
-  Foreign Key (department) REFERENCES department(id) INTEGER NOT NULL
+  department_id INTEGER NOT NULL,
+  Foreign Key (department_id) REFERENCES department(id) 
 )
 
 CREATE TABLE employee (
   id SERIAL PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  Foreign Key (role_id) REFERENCES role(id) INTEGER NOT NULL,
-  FOREIGN KEY (manager_id) REFERENCES employee(id) INTEGER
+  role_id INTEGER NOT NULL,
+  Foreign Key (role_id) REFERENCES role(id),
+  manager_id INTEGER,
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
 )
